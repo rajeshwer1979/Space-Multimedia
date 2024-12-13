5b26463b9eb9c7d491adec445ffa1bd3744d027b
Rajeshwer <31434825+rajeshwer1979@users.noreply.github.com>
2024-12-13 06:08:58 +0530

    oojs-final structured
    oojs-final structured Learning

---
+++ oojs-finalstructured(28-5-14)/app.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+window.addEventListener("load",onLoad);
+
+
+
+function onLoad(event){
+
+
+	var l = new List();
+	l.setData([{title:"dsfdsfdsf"},{title:"dsfdsfdsf"}]);
+	l.setData([{title:"dsfdsfdsf"},{title:"dsfdsfdsf"}]);
+	l.setData([{title:"dsfdsfdsf"},{title:"dsfdsfdsf"}]);
+	l.render(document.getElementById("right"));
+
+	
+	
+}
+
+
---
+++ oojs-finalstructured(28-5-14)/com/abc/view/AdvancedItem.js
@@ -0,0 +1,4 @@
+// JavaScript Document
+function AdvancedItem(){
+	this.__proto__ = new ListItem();
+}
